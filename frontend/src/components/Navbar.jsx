8e7919e38c090c5cc132fca07801fbd4ea3ce153
Ali <ha5403905@gmail.com>
2025-09-23 15:56:51 +0500

    icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,9 @@
-import { Link, useLocation } from "react-router";
+import { Link } from "react-router";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
 
 const Navbar = () => {
-  const location = useLocation();
-  const isChatPage = location.pathname?.startsWith("/chat");
-
   const { logoutMutation } = useLogout();
 
   return (
@@ -14,16 +11,14 @@ const Navbar = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-end w-full">
           {/* LOGO - ONLY IN THE CHAT PAGE */}
-          {isChatPage && (
-            <div className="pl-5">
-              <Link to="/" className="flex items-center gap-2.5">
-                <ShipWheelIcon className="size-9 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  DevLink
-                </span>
-              </Link>
-            </div>
-          )}
+          <div className="pl-5 lg:hidden md:block">
+            <Link to="/" className=" flex items-center gap-2.5">
+              <ShipWheelIcon className="size-9 text-primary" />
+              <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                DevLink
+              </span>
+            </Link>
+          </div>
 
           <div className="flex items-center gap-3 sm:gap-4 ml-auto">
             <Link to={"/notifications"}>
